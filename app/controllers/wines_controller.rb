ac76b4b1ca1c13cde6655a48de539c4203d195c0
Jessica Paczuski <jz.paczuski@gmail.com>
2016-04-11 20:23:54 +0200

    fix wines_controller'

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -3,7 +3,7 @@ class WinesController < ApplicationController
 
   # GET /wines
   # GET /wines.json
-  def index 
+  def index
     if params[:search]
       wine = Wine.find_by_name(params[:search])
       if wine.present?
@@ -87,7 +87,7 @@ class WinesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_wine
-      @wines = Wine.find(params[:id])
+      @wine = Wine.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
